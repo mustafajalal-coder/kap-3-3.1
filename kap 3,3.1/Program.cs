130873910324a622cc5c8909267ab0d2112c445a
muja07001 <muja07001@E-PW05CGR9>
2024-11-07 15:38:21 +0100

    Add project files.

---
+++ kap 3,3.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kap_3_3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur gammal är du? (år)");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder < 16)
+
+            {
+                Console.WriteLine("du får inte vara med");
+            }
+
+            if (ålder > 19)
+            {
+                Console.WriteLine("du får inte vara med ");
+
+            }
+
+            else;
+            Console.WriteLine("du får vara med");
+
+              
+            
+
+
+        }
+    }
+}
